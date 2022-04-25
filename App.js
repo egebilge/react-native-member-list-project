65842693d4b43b887fe6fffb92fa42dde1558b05
egebilge <orhanebilge07@gmail.com>
2022-04-25 15:56:20 +0300

    ...

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-import Home from './src/screens/Home';
 import Login from './src/screens/Login';
 import Member from './src/screens/Member';
 import {Provider} from 'react-redux';
@@ -34,7 +33,6 @@ function App() {
               headerShown: false,
             }}
           />
-          <Stack.Screen name="Home" component={Home} />
           <Stack.Screen name="My Member List" component={Member} />
           <Stack.Screen name="ADD Member" component={CreateMember} />
         </Stack.Navigator>
